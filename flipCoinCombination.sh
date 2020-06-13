1221bda4fd7fa23caa781a7bdb8ffade527b8428
manojsid <siddammanoj1998@gmail.com>
2020-06-13 15:44:48 +0530

    Flip commited

--- flipCoinCombination.sh
+++ flipCoinCombination.sh
@@ -1,2 +1,12 @@
 echo "welcome to flipcoin Combination problem"
 echo "---------------------------------------"
+heads=0;
+tails=1;
+Flip=$(($RANDOM%2))
+if [[ $Flip -eq $heads ]]; then
+	#statements
+	echo "Coin flipped to Heads"
+
+else
+	echo "Coin flipped to Tails"
+fi
